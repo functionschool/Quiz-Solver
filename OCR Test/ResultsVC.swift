86f1c95890321fa95434a59d72d4232959740015
Kousei Richeson <kousei@nmsu.edu>
2018-05-02 23:33:56 -0600

    Added Quizlet extractor

--- OCR Test/ResultsVC.swift
+++ OCR Test/ResultsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import Kanna
+import Foundation
 import UIKit
 
 class ResultsVC: UIViewController {
@@ -20,10 +21,7 @@ class ResultsVC: UIViewController {
     // --------------------------------------------------------------
     // MARK:- Variables
     // --------------------------------------------------------------
-    var boldedQuery: [[String]] = []
-    var html: String?
     var question: String?
-    var urlLinks: [String] = []
     
     
     // --------------------------------------------------------------
@@ -32,10 +30,15 @@ class ResultsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var boldedQuery: [[String]] = []
+        var urlList: [String] = []
+        var html: String?
+        
+        
         if(question == nil) {
             print("Using default Question")
-            question = "the main suite of protocols used on the internet is ________."
-            // question = "what does API stand for"
+            // question = "the main suite of protocols used on the internet is ________."
+            question = "what does API stand for"
         }
         
         questionLabel.text = question
@@ -46,40 +49,42 @@ class ResultsVC: UIViewController {
             return
         }
         
-        urlLinks = extractLinks()
-        boldedQuery = extractBolds()
+        urlList = extractLinks(html: html!)
+        boldedQuery = extractBolds(html: html!)
         
-        print()
-        print()
-        for i in stride(from: 0, to: urlLinks.count, by: 1) {
-            print(i, terminator: ". ")
-            print(urlLinks[i])
-        }
-        print()
-        print()
-        for i in stride(from: 0, to: boldedQuery.count, by: 1) {
-            print(i, terminator:". ")
-            print(boldedQuery[i])
-        }
-        
-        let relevantTuple = removeIrrelevants(urls: urlLinks, bolds: boldedQuery)
-        urlLinks = relevantTuple.0
+        let relevantTuple = removeIrrelevants(urls: urlList, bolds: boldedQuery)
+        urlList = relevantTuple.0
         boldedQuery = relevantTuple.1
         
+        let proritiyTuple = prioritizeList(urls: urlList, bolds: boldedQuery)
+        urlList = proritiyTuple.0
+        boldedQuery = proritiyTuple.1
+        
         print()
         print()
-        for i in stride(from: 0, to: urlLinks.count, by: 1) {
+        for i in stride(from: 0, to: urlList.count, by: 1) {
             print(i, terminator: ". ")
-            print(urlLinks[i])
-        }
-        print()
-        print()
-        for i in stride(from: 0, to: boldedQuery.count, by: 1) {
-            print(i, terminator:". ")
-            print(boldedQuery[i])
+            print(urlList[i])
         }
         
-        prioritizeList(urls: urlLinks, bolds: boldedQuery)
+        let searchQueries = matrixToArray(matrix: boldedQuery)
+        
+        print()
+        print()
+        for i in stride(from: 0, to: searchQueries.count, by: 1) {
+            print(i, terminator:". ")
+            print(searchQueries[i])
+        }
+        
+        html = quizletSearch(searchFor: searchQueries[0], url: urlList[0])
+        
+        if(html == nil) {
+            print("Cannot Continue")
+            return
+        }
+        
+        let potentialAnswer = extractAnswer(html: html!, searchFor: searchQueries[0])
+        print(potentialAnswer ?? "Nil")
         
     }
     
@@ -109,46 +114,73 @@ class ResultsVC: UIViewController {
         }
     }
     
+    func quizletSearch(searchFor query: String, url quizletUrl: String) -> String? {
+        print("Attempting to fetch from: " + quizletUrl)
+        guard let URL = URL(string: quizletUrl) else {
+            print("Error: \(quizletUrl) doesn't seem to be a valid URL")
+            return nil
+        }
+        do {
+            let myHTMLString = try String(contentsOf: URL, encoding: .ascii)
+            return myHTMLString
+        } catch let error {
+            print("Error: \(error)")
+            return nil
+        }
+    }
+    
+    func matrixToArray(matrix: [[String]]) -> [String] {
+        var array = [String]()
+        for i in stride(from: 0, to: matrix.count, by: 1) {
+            var score = 0
+            var insertThis = ""
+            for j in stride(from: 0, to: matrix[i].count, by: 1) {
+                let element = matrix[i][j]
+                let elementWordCount = element.split(separator: " ").count
+                if(score < elementWordCount) {
+                    insertThis = element
+                    score = elementWordCount
+                }
+            }
+            array.append(insertThis)
+        }
+        return array
+    }
+    
     func prioritizeList(urls: [String], bolds: [[String]]) -> ([String], [[String]]) {
-        let urlList = urls
+        var urlList = urls
         var boldList = bolds
-        var indexTracker = [Int]()
+        var scoreTracker = [Float]()
         
         for i in stride(from: 0, to: boldList.count, by: 1) {
-            var score = 0
+            var score: Float = 0.0
             for j in stride(from: 0, to: boldList[i].count, by: 1) {
                 let result = boldList[i][j]
                 let resultWordCount = result.split(separator: " ").count
-                if(score < resultWordCount) {
-                    score = resultWordCount
+                if(score < Float(resultWordCount)) {
+                    score = Float(resultWordCount)
                 }
             }
-            indexTracker.append(score)
+            score = score + Float(boldList[i].count-1) * 0.1
+            scoreTracker.append(score)
         }
         
-        print()
-        print(indexTracker)
-        
-        print()
-        let hi = mergeSort(indexTracker, urlList)
-        print(hi.0)
-        for i in stride(from: 0, to: hi.1.count, by: 1) {
-            print(i, terminator:". ")
-            print(hi.1[i])
-        }
+        let tuple = mergeSort(scoreTracker, urlList, boldList)
+        urlList = tuple.1
+        boldList = tuple.2
 
         return (urlList, boldList)
     }
     
-    func mergeSort(_ scoreArray: [Int], _ urlArray: [String]) -> ([Int], [String]) {
+    func mergeSort(_ scoreArray: [Float], _ urlArray: [String], _ boldMatrix: [[String]]) -> ([Float], [String], [[String]]) {
         
         // Base Case: If array has more than 1 element, continue.
-        guard scoreArray.count > 1 else { return (scoreArray, urlArray) }
+        guard scoreArray.count > 1 else { return (scoreArray, urlArray, boldMatrix) }
         
         let middleIndex = scoreArray.count / 2
         
-        let leftTuple = mergeSort(Array(scoreArray[0..<middleIndex]), Array(urlArray[0..<middleIndex]))
-        let rightTuple = mergeSort(Array(scoreArray[middleIndex..<scoreArray.count]), Array(urlArray[middleIndex..<urlArray.count]))
+        let leftTuple = mergeSort(Array(scoreArray[0..<middleIndex]), Array(urlArray[0..<middleIndex]), Array(boldMatrix[0..<middleIndex]))
+        let rightTuple = mergeSort(Array(scoreArray[middleIndex..<scoreArray.count]), Array(urlArray[middleIndex..<urlArray.count]), Array(boldMatrix[middleIndex..<boldMatrix.count]))
         
         let leftScoreArray = leftTuple.0
         let rightScoreArray = rightTuple.0
@@ -156,36 +188,46 @@ class ResultsVC: UIViewController {
         let leftUrlArray = leftTuple.1
         let rightUrlArray = rightTuple.1
         
+        let leftBoldMatrix = leftTuple.2
+        let rightBoldMatrix = rightTuple.2
+        
         // Recursive call.
-        return merge(leftScoreArray, rightScoreArray, leftUrlArray, rightUrlArray)
+        return merge(leftScoreArray, rightScoreArray, leftUrlArray, rightUrlArray, leftBoldMatrix, rightBoldMatrix)
     }
     
-    func merge(_ leftScoreArray: [Int], _ rightScoreArray: [Int], _ leftUrlArray: [String], _ rightUrlArray: [String]) -> ([Int], [String]) {
+    func merge(_ leftScoreArray: [Float], _ rightScoreArray: [Float], _ leftUrlArray: [String], _ rightUrlArray: [String], _ leftBoldMatrix: [[String]], _ rightBoldMatrix: [[String]]) -> ([Float], [String], [[String]]) {
         var leftIndex = 0
         var rightIndex = 0
-        var orderedScoreArray: [Int] = []
+        var orderedScoreArray: [Float] = []
         var orderedUrlArray: [String] = []
+        var orderedBoldMatrix: [[String]] = []
         
         while leftIndex < leftScoreArray.count && rightIndex < rightScoreArray.count {
             let leftScoreElement = leftScoreArray[leftIndex]
             let rightScoreElement = rightScoreArray[rightIndex]
             let leftUrlElement = leftUrlArray[leftIndex]
             let rightUrlElement = rightUrlArray[rightIndex]
+            let leftBoldArray = leftBoldMatrix[leftIndex]
+            let rightBoldArray = rightBoldMatrix[rightIndex]
             
             if leftScoreElement > rightScoreElement {
                 orderedScoreArray.append(leftScoreElement)
                 orderedUrlArray.append(leftUrlElement)
+                orderedBoldMatrix.append(leftBoldArray)
                 leftIndex += 1
             } else if leftScoreElement < rightScoreElement {
                 orderedScoreArray.append(rightScoreElement)
                 orderedUrlArray.append(rightUrlElement)
+                orderedBoldMatrix.append(rightBoldArray)
                 rightIndex += 1
             } else {
                 orderedScoreArray.append(leftScoreElement)
                 orderedUrlArray.append(leftUrlElement)
+                orderedBoldMatrix.append(leftBoldArray)
                 leftIndex += 1
                 orderedScoreArray.append(rightScoreElement)
                 orderedUrlArray.append(rightUrlElement)
+                orderedBoldMatrix.append(rightBoldArray)
                 rightIndex += 1
             }
         }
@@ -193,34 +235,36 @@ class ResultsVC: UIViewController {
         while leftIndex < leftScoreArray.count {
             orderedScoreArray.append(leftScoreArray[leftIndex])
             orderedUrlArray.append(leftUrlArray[leftIndex])
+            orderedBoldMatrix.append(leftBoldMatrix[leftIndex])
             leftIndex += 1
         }
         
         while rightIndex < rightScoreArray.count {
             orderedScoreArray.append(rightScoreArray[rightIndex])
             orderedUrlArray.append(rightUrlArray[rightIndex])
+            orderedBoldMatrix.append(rightBoldMatrix[rightIndex])
             rightIndex += 1
         }
         
-        return (orderedScoreArray, orderedUrlArray)
+        return (orderedScoreArray, orderedUrlArray, orderedBoldMatrix)
         
     }
     
     func removeIrrelevants(urls: [String], bolds: [[String]]) -> ([String], [[String]]) {
         var removeTheseIndexes = [Int]()
-        var urlList = urls
+        var urlLinks = urls
         var boldList = bolds
         
         for i in stride(from: 0, to: urlLinks.count, by: 1) {
-            if (urlList[i].range(of: "...") != nil) {
+            if (urlLinks[i].range(of: "...") != nil) {
                 removeTheseIndexes.append(i)
             }
-            if (urlList[i].range(of: "quizlet.com") == nil) {
+            if (urlLinks[i].range(of: "quizlet.com/") == nil) {
                 removeTheseIndexes.append(i)
             }
         }
         
-        urlList = urlList.enumerated().filter {
+        urlLinks = urlLinks.enumerated().filter {
             !removeTheseIndexes.contains($0.offset)
         }.map { $0.element }
         
@@ -228,12 +272,34 @@ class ResultsVC: UIViewController {
             !removeTheseIndexes.contains($0.offset)
             }.map { $0.element }
         
-        return (urlList, boldList)
+        return (urlLinks, boldList)
     }
     
-    func extractBolds() -> [[String]] {
+    func extractAnswer(html: String, searchFor query: String) -> String? {
+        if let doc = try? HTML(html: html, encoding: .utf8) {
+            let xPathSet = "//div[@class='SetPageTerm-content']"
+            for set in doc.xpath(xPathSet) {
+                if let doc2 = try? HTML(html: set.toHTML!, encoding: .utf8) {
+                    var pairArray = [String]()
+                    let xPathPair = "//span[@class='TermText notranslate lang-en']"
+                    for pair in doc2.xpath(xPathPair) {
+                        pairArray.append(pair.text!)
+                    }
+                    if(pairArray[0].range(of: query) != nil) {
+                        return pairArray[1]
+                    }
+                    if(pairArray[1].range(of: query) != nil) {
+                        return pairArray[0]
+                    }
+                }
+            }
+        }
+        return nil
+    }
+    
+    func extractBolds(html: String) -> [[String]] {
         var allBolds: [[String]] = []
-        if let doc = try? HTML(html: html!, encoding: .utf8) {
+        if let doc = try? HTML(html: html, encoding: .utf8) {
             let xPath = "//span[@class='st']"
             for item in doc.xpath(xPath) {
                 var bolds: [String] = []
@@ -249,9 +315,9 @@ class ResultsVC: UIViewController {
         return allBolds
     }
     
-    func extractLinks() -> [String] {
+    func extractLinks(html: String) -> [String] {
         var links: [String] = []
-        if let doc = try? HTML(html: html!, encoding: .utf8) {
+        if let doc = try? HTML(html: html, encoding: .utf8) {
             let xPath = "//div[@class='hJND5c']/cite"
             // For Bing:
             // let xPath = "//div[@class='b_attribution']/cite"
